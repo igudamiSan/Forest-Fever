2afb139f36e1c80b1ce94743071bfc750dcca170
Elizabeth Anna John <elizabethannajohn@gmail.com>
2023-12-29 13:51:31 +0530

    Random
    very random

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //oolalalalalala
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
