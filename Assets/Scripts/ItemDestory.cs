9ee04f60050d30284dfa7d41cae643f8430de27c
Elizabeth Anna John <elizabethannajohn@gmail.com>
2023-12-29 14:30:43 +0530

    Random
    this is very random

---
+++ Assets/Scripts/ItemDestory.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if u see this say benedict cucumberbach
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
